6be3297b28e67c3ba548a108e890f757d262da0e
Dral <m.c.dral@gmail.com>
2016-03-24 03:42:38 +0100

    🍉 Add expandable markers on the sides of the board

--- source/go/GoBoard.js
+++ source/go/GoBoard.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import {View} from '../components/base'
-import {withState} from 'recompose'
-import {random} from 'lodash'
+import {withState, compose} from 'recompose'
+import {random, range} from 'lodash'
 
 import {
-  board, stenen,
+  board, board_background, grid, stenen,
   zwart_aan_zet, wit_aan_zet,
   not_my_turn,
+  bottom_marker, right_marker, enable_markers,
 } from './Board.css'
 
 import Grid from '../components/Grid'
 import Stone from './Stone'
+
+let ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
 
 let getStone = (b, x, y, defaultStone) => {
   let row = (b || [])[x]
@@ -20,11 +23,16 @@ let getStone = (b, x, y, defaultStone) => {
 }
 
 let noShake = {x: 0, y: 0}
-let Board = withState('shake', 'setShake', noShake, ({
+let Board = compose(
+  withState('shake', 'setShake', noShake),
+  withState('showMarkers', 'setShowMarkers', false)
+)(({
   shake, setShake,
   value, previewBoard, onMove,
   onPreview, onClearPreview,
   lastMove, turn, color,
+
+  showMarkers, setShowMarkers,
 }) => {
   let shakeIt = () => {
     setShake({
@@ -39,16 +47,35 @@ let Board = withState('shake', 'setShake', noShake, ({
   let colorClassName = color === 'black' ? zwart_aan_zet : wit_aan_zet
   return (
     <View
-      className={board + ' ' + (turn ? colorClassName : not_my_turn)}
+      className={[
+        board,
+        (turn ? colorClassName : not_my_turn),
+        showMarkers && enable_markers,
+      ].filter(Boolean).join(' ')}
       /*style={{
         transform: 'translate3d(0, 0, 0)',
         marginTop: shake.x,
         marginLeft: shake.y,
       }}*/
     >
+      <View className={board_background} />
+
+      <View className={bottom_marker} onPress={() => setShowMarkers(!showMarkers)}>
+        { ALPHABET.slice(0, value.length).map(letter =>
+          <View key={letter}>{letter}</View>
+        )}
+      </View>
+
+      <View className={right_marker} onPress={() => setShowMarkers(!showMarkers)}>
+        { range(0, value.length).map(digit =>
+          <View key={digit}>{digit + 1}</View>
+        )}
+      </View>
+
       <Grid
         width={value.length + 1}
         height={value.length + 1}
+        className={grid}
         style={{
           transform: 'translate(25px, 25px)',
         }}
